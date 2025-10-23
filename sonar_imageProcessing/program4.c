69d736aa2a57d4157a04cf19e93f9dbd2687dd4e
yashbosss <ydarji30@gmail.com>
2025-10-23 14:55:45 +0530

    review comments addressed

--- sonar_imageProcessing/program4.c
+++ sonar_imageProcessing/program4.c
@@ -1,110 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void transposeMatrix(int* sonarImageMatrix[][10], int matrixSize) {
-    for (int i = 0; i < matrixSize; i++) {
-        for (int j = i + 1; j < matrixSize; j++) {
-            int* temp = sonarImageMatrix[i][j];
-            sonarImageMatrix[i][j] = sonarImageMatrix[j][i];
-            sonarImageMatrix[j][i] = temp;
+void transposeMatrix(int **sonarImageMatrix, int matrixSize)
+{
+    for (int i = 0; i < matrixSize; i++)
+    {
+        for (int j = i + 1; j < matrixSize; j++)
+        {
+            int temp = *(*(sonarImageMatrix + i) + j);
+            *(*(sonarImageMatrix + i) + j) = *(*(sonarImageMatrix + j) + i);
+            *(*(sonarImageMatrix + j) + i) = temp;
         }
     }
 }
 
-void reverseMatrixRows(int* sonarImageMatrix[][10], int matrixSize) {
-    for (int i = 0; i < matrixSize; i++) {
-        for (int j = 0; j < matrixSize / 2; j++) {
-            int* temp = sonarImageMatrix[i][j];
-            sonarImageMatrix[i][j] = sonarImageMatrix[i][matrixSize - 1 - j];
-            sonarImageMatrix[i][matrixSize - 1 - j] = temp;
+void reverseMatrixRows(int **sonarImageMatrix, int matrixSize)
+{
+    for (int i = 0; i < matrixSize; i++)
+    {
+        int *rowStart = *(sonarImageMatrix + i);
+        int *rowEnd = *(sonarImageMatrix + i) + matrixSize - 1;
+
+        while (rowStart < rowEnd)
+        {
+            int temp = *rowStart;
+            *rowStart = *rowEnd;
+            *rowEnd = temp;
+            rowStart++;
+            rowEnd--;
         }
     }
 }
 
-void rotate90Clockwise(int* sonarImageMatrix[][10], int matrixSize) {
+void rotate90Clockwise(int **sonarImageMatrix, int matrixSize)
+{
     transposeMatrix(sonarImageMatrix, matrixSize);
     reverseMatrixRows(sonarImageMatrix, matrixSize);
 }
 
-void smoothMatrix(int* sonarImageMatrix[][10], int matrixSize) {
-    for (int i = 0; i < matrixSize; i++) {
-        for (int j = 0; j < matrixSize; j++) {
-            int sum = 0, count = 0;
+void SmoothingFilter(int **sonarImageMatrix, int matrixSize)
+{
+    for (int i = 0; i < matrixSize; i++)
+    {
+        for (int j = 0; j < matrixSize; j++)
+        {
+            int sumOfNeighbour = 0, countNeighbour = 0;
 
-            for (int di = -1; di <= 1; di++) {
-                for (int dj = -1; dj <= 1; dj++) {
-                    int ni = i + di;
-                    int nj = j + dj;
-                    if (ni >= 0 && ni < matrixSize && nj >= 0 && nj < matrixSize) {
-                        sum += *sonarImageMatrix[ni][nj] % 1000;
-                        count++;
+            for (int x = i - 1; x <= i + 1; x++)
+            {
+                for (int y = j - 1; y <= j + 1; y++)
+                {
+                    if (x >= 0 && x < matrixSize && y >= 0 && y < matrixSize)
+                    {
+                        sumOfNeighbour += *(*(sonarImageMatrix + x) + y) % 1000;
+                        countNeighbour++;
                     }
                 }
             }
 
-            int avg = sum / count;
-            *sonarImageMatrix[i][j] += avg * 1000;
+            *(*(sonarImageMatrix + i) + j) += (sumOfNeighbour / countNeighbour) * 1000;
         }
     }
 
-    for (int i = 0; i < matrixSize; i++) {
-        for (int j = 0; j < matrixSize; j++) {
-            *sonarImageMatrix[i][j] = *sonarImageMatrix[i][j] / 1000;
+    for (int i = 0; i < matrixSize; i++)
+    {
+        for (int j = 0; j < matrixSize; j++)
+        {
+            *(*(sonarImageMatrix + i) + j) /= 1000;
         }
     }
 }
 
-void printMatrix(int* sonarImageMatrix[][10], int matrixSize) {
-    for (int i = 0; i < matrixSize; i++) {
-        for (int j = 0; j < matrixSize; j++) {
-            printf("%3d ", *sonarImageMatrix[i][j]);
+void printMatrix(int **sonarImageMatrix, int matrixSize)
+{
+    for (int i = 0; i < matrixSize; i++)
+    {
+        for (int j = 0; j < matrixSize; j++)
+        {
+            printf("%4d", *(*(sonarImageMatrix + i) + j));
         }
         printf("\n");
     }
 }
 
-void freeMatrix(int* sonarImageMatrix[][10], int matrixSize) {
-    for (int i = 0; i < matrixSize; i++) {
-        for (int j = 0; j < matrixSize; j++) {
-            free(sonarImageMatrix[i][j]);
-        }
+void freeMatrix(int **sonarImageMatrix, int matrixSize)
+{
+    for (int i = 0; i < matrixSize; i++)
+    {
+        free(*(sonarImageMatrix + i));
     }
+    free(sonarImageMatrix);
 }
 
-int main() {
+int main()
+{
     int sonarImageSize;
     printf("Enter size of sonar Image between 2 to 10:\n");
     scanf("%d", &sonarImageSize);
 
-    int* sonarImageMatrix[10][10];
-    int intensityValue, checkInputBuffer;
+    int **sonarImageMatrix = (int **)malloc(sonarImageSize * sizeof(int *));
+    for (int i = 0; i < sonarImageSize; i++)
+    {
+        *(sonarImageMatrix + i) = (int *)malloc(sonarImageSize * sizeof(int));
+    }
 
-    for (int i = 0; i < sonarImageSize; i++) {
-        for (int j = 0; j < sonarImageSize; j++) {
-            while (1) {
-                printf("Enter intensityValue for index [%d][%d] : ", i, j);
-                if (scanf("%d", &intensityValue) == 1 && intensityValue >= 0 && intensityValue <= 255) {
-                    while ((checkInputBuffer = getchar()) != '\n' && checkInputBuffer != EOF);
-                    sonarImageMatrix[i][j] = (int*)malloc(sizeof(int));
-                    *sonarImageMatrix[i][j] = intensityValue;
-                    break;
-                } else {
-                    printf("Invalid input. Try again.\n");
-                    while ((checkInputBuffer = getchar()) != '\n' && checkInputBuffer != EOF);
-                }
-            }
+    srand(time(NULL));
+
+    for (int i = 0; i < sonarImageSize; i++)
+    {
+        for (int j = 0; j < sonarImageSize; j++)
+        {
+            *(*(sonarImageMatrix + i) + j) = rand() % 256;
         }
     }
 
-    printf("\nOriginal:\n");
+    printf("\nOriginal Matrix:\n");
     printMatrix(sonarImageMatrix, sonarImageSize);
 
     rotate90Clockwise(sonarImageMatrix, sonarImageSize);
     printf("\nRotated:\n");
     printMatrix(sonarImageMatrix, sonarImageSize);
 
-    smoothMatrix(sonarImageMatrix, sonarImageSize);
-    printf("\nSmoothing Filter:\n");
+    SmoothingFilter(sonarImageMatrix, sonarImageSize);
+    printf("\nSmoothening Filter:\n");
     printMatrix(sonarImageMatrix, sonarImageSize);
 
     freeMatrix(sonarImageMatrix, sonarImageSize);
